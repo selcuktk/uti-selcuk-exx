a02473c5ddf5b5a726a4142985a898dd59ce6a3a
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-07-20 17:47:08 +0300

    feat: response response functions added for gray and climate

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,28 @@
-
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
+from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, GrayOutputs, \
+    GrayResponse, GrayExecutor, ClimateOutputs, ClimateResponse, ClimateExecutor, OutputImageOne, OutputImageTwo
 
 
-def build_response(context):
-    outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
+def build_response_climate(context):
+    outputImageOne = OutputImageOne(value=context.image)
+    outputImageTwo = OutputImageTwo(value=context.image)
+    Outputs = ClimateOutputs(outputImageOne=outputImageOne, outputImageTwo=outputImageTwo)
+    climateResponse = ClimateResponse(outputs=Outputs)
+    climateExecutor = ClimateExecutor(value=climateResponse)
+    executor = ConfigExecutor(value=climateExecutor)
+    packageConfigs = PackageConfigs(executor=executor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
+
+
+
+def build_response_gray(context):
+    outputImageOne = OutputImageOne(value=context.image)
+    Outputs = GrayOutputs(outputImageOne=outputImageOne)
+    grayResponse = GrayResponse(outputs=Outputs)
+    grayExecutor = GrayExecutor(value=grayResponse)
+    executor = ConfigExecutor(value=grayExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
